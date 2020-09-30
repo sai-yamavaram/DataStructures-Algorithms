f5b28ec6b7a83bdbbce0d5601c1619fdece282f4
shivani-yamavaram <sreeshivanijuly07@gmail.com>
2020-09-30 22:20:45 +0530

    comments removed

--- Famous-Coding-Interview-Problems/OopsAllConcepts.cpp
+++ Famous-Coding-Interview-Problems/OopsAllConcepts.cpp
@@ -1,4 +1,4 @@
-//Video explanation at : https://youtu.be/qsMahidcehY 
+
 #include <iostream>
 using namespace std;
 
@@ -22,9 +22,6 @@ int main()
 {
     A a, b; //local --> stack
     A c, d, e;
-    
-    //dynamic --> heap
-    //A* ptr = (A*)malloc(sizeof(A)); //allocates memory, doesnt call ctor
     A* ptr = new A; //ctor as well
     A* ptr2 = new A;
     //free(ptr); //doesn't call the destructor
